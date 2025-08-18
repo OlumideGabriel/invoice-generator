13f30be7943b0d98874fbbba8324d726cf99a72a
Olumide Ogunseye <54401201+OlumideGabriel@users.noreply.github.com>
2025-08-18 18:16:16 +0100

    z-index

--- frontend/src/components/SideMenu.tsx
+++ frontend/src/components/SideMenu.tsx
@@ -167,7 +167,7 @@ const SideMenu: React.FC = () => {
     </div>
 
     {/* Mobile Sidebar */}
-    <div className="md:hidden fixed bottom-0 left-0 right-0 bg-neutral-50 border-t border-t-1 border-t-gray-200 border-t-dashed z-1000">
+    <div className="md:hidden fixed bottom-0 left-0 right-0 bg-neutral-50 border-t border-t-1 border-t-gray-200 border-t-dashed z-50">
       <nav className="flex justify-around items-center mt-1">
         {mobileMenuItems.map(({ path, label, icon }) => (
           <Link
