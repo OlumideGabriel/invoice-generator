6ffefb02ded8dfec447ccf8aaf4b8563791fefdc
Olumide Ogunseye <54401201+OlumideGabriel@users.noreply.github.com>
2025-09-12 07:18:56 +0100

    homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,19 +9,43 @@ const Home = () => {
     const { user, openAuthModal } = useAuth();
 
     return (
-        <div className="h-full bg-[#f7f3ec] max-h-100dvh text-gray-900 overflow-x-hidden justify-between flex flex-col">
-            <MainMenu background="bg-[#f7f3ec]" />
+        <div className="h-full bg-white max-h-100dvh text-gray-900 overflow-x-hidden justify-between flex flex-col">
+            <MainMenu background="bg-white" />
             <section className="flex items-center mx-auto px-4 md:px-16">
                 <div className="flex flex-col md:flex-row items-center w-full mx-auto">
                     {/* Hero Content */}
-                    <div className="flex-1 max-w-lg md:max-w-xl text-center">
-                        <h1 className="text-5xl md:text-7xl lg:text-7xl font-normal leading-none mb-10">
-                            Modern Invoice Management
+                    <div className="flex-1 max-w-lg md:max-w-2xl text-center">
+                    <span className="text-md font-medium text-gray-500 mb-4 inline-block">
+                    <div className="avatar avatar-sm mr-2 inline-block">
+                        <img
+                            src="https://plus.unsplash.com/premium_photo-1671656349218-5218444643d8?q=80&w=987&auto=format&fit=crop&ixlib=rb-
+                            4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            alt="Envoyce Logo"
+                            className="rounded-full object-cover w-6 h-6 md:w-9 md:h-9 inline-block border-2 border-white"
+
+                        />
+                        <img
+                            src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=1480&auto=format&fit=crop&ixlib=rb-
+                            4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            alt="Envoyce Logo"
+                            className="rounded-full w-6 h-6 md:w-9 md:h-9 inline-block -ml-2 border-2 border-white"
+                        />
+                        <img
+                            src="https://images.unsplash.com/photo-1620075225255-8c2051b6c015?q=80&w=1494&auto=format&fit=crop&ixlib=rb-
+                            4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            alt="Envoyce Logo"
+                            className="rounded-full w-6 h-6 md:w-9 md:h-9 inline-block -ml-2 border-2 border-white"
+                        />
+                    </div>
+                        Already used by 597+ freelancers worldwide&nbsp;
+                        </span>
+                        <h1 className="text-5xl font-normal leading-none mb-4">
+                            A Simpler Way to Invoice
                         </h1>
                         <p className="text-xl md:text-2xl text-gray-600 mb-12 leading-relaxed ">
-                            Envoyce provides modern tools to <span className="standout bg-[#c7f9cc]">create</span>&nbsp;
-                            <span className="standout bg-[#fae588]">manage </span>
-                             and <span className="standout bg-[#a2d2ff]">share</span> professional invoices with ease.
+                            Envoyce provides tools to help you <span className="standout bg-[#c7f9cc]">create</span>&nbsp;
+                            <span className="standout bg-[#fae588]">manage</span>&nbsp;
+                             and <span className="standout bg-[#a2d2ff]">share</span> professional invoices seamlessly.
                         </p>
                         <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-center">
                             <button
@@ -40,14 +64,17 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <svg>
-                <defs>
-                    <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-                        <stop offset="0%" stopColor="#34D399" />
-                        <stop offset="100%" stopColor="#10B981" />
-                    </linearGradient>
-                </defs>
-            </svg>
+
+            <div className="relative bottom-0 max-w-5xl w-full mx-auto mt-6 border-b-1 border-white overflow-hidden rounded-t-3xl">
+  <img
+    src="https://images.unsplash.com/photo-1755541516450-644adb257ad0?q=80&w=1480&auto=format&fit=crop&ixlib=rb-
+    4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    className="w-full h-64 md:h-80 object-cover object-[90%_25%] hover:scale-105 transition-transform duration-300"
+  />
+
+</div>
+
+
 
             <Footer />
         </div>
