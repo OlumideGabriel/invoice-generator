0fdf51a75659ad6b162723bedba606d6bbac629f
Olumide Ogunseye <54401201+OlumideGabriel@users.noreply.github.com>
2025-09-15 20:34:16 +0100

    zoom

--- frontend/src/components/InvoiceSidebar.tsx
+++ frontend/src/components/InvoiceSidebar.tsx
@@ -49,7 +49,7 @@ const InvoiceSidebar: React.FC<InvoiceSidebarProps> = ({
   return (
       <>
       {previewPdfUrl && (
-  <div className="fixed inset-0 bg-black px-2 flex md:grid bg-opacity-50 items-center justify-center z-50">
+  <div className="fixed inset-0 bg-black px-2 flex md:grid bg-opacity-70 items-center justify-center z-50">
 
     <div className="bg-white/40 rounded-lg shadow-lg w-full from-neutral-900 to-neutral-300
     md:min-w-[600px] sm:min-w-[500px] min-w-full h-auto relative flex flex-col">
@@ -68,7 +68,7 @@ const InvoiceSidebar: React.FC<InvoiceSidebarProps> = ({
       <div className="flex justify-center items-center w-full overflow-hidden">
         <h1 className="sr-only">Invoice Preview Modal</h1>
         <iframe
-          src={`${previewPdfUrl}#toolbar=0&navpanes=0&scrollbar=0&zoom=40`}
+          src={`${previewPdfUrl}#toolbar=0&navpanes=0&scrollbar=0&zoom=20`}
           title="Invoice Preview"
           className="aspect-[85/110] w-full !bg-neutral-800 rounded-lg p-2"
         />
