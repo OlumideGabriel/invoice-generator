2af45060353c5330dd94fdd2fb7b137dc711f595
Olumide Ogunseye <54401201+OlumideGabriel@users.noreply.github.com>
2025-09-18 17:15:13 +0100

    updated the advert

--- frontend/src/components/Advert.tsx
+++ frontend/src/components/Advert.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { X, Crown} from 'lucide-react';
+import { X, Crown, Plus} from 'lucide-react';
 import { useNavigate } from "react-router-dom";
 
 export default function Advert() {
@@ -11,9 +11,10 @@ export default function Advert() {
   }
 
   return (
-      <a href="https://medium.com/@talktoolumide/it-started-with-a-familiar-pain-point-f33f26b85d3a" target="_blank" rel="noopener noreferrer" className="flex items-start z-10 w-full">
+      <a href="https://medium.com/@talktoolumide/it-started-with-a-familiar-pain-point-f33f26b85d3a" target="_blank" rel="noopener noreferrer" className="flex items-start z-10 w-full hover:contrast-125">
     <div className="flex items-start z-10 w-full">
-      <div className="bg-[#8eda91] hover:bg-[#8eda91]/60 shadow-xs hidden xl:block p-4 hover:shadow-sm transition-all duration-500 ease-in-out relative">
+      <div className="w-full">
+      <div className="bg-[#8eda91] hover:bg-[#8eda91] shadow-xs hidden xl:block p-4 transition-all duration-200 ease-in-out relative">
         {/* Close Button */}
         <button
           onClick={() => setIsVisible(false)}
@@ -24,26 +25,34 @@ export default function Advert() {
         </button>
 
         {/* Header with icon */}
-        <div className="flex items-center gap-2 mb-3 pr-6">
-          <h3 className="font-semibold text-[#0e423e]">We Got 500+ Users</h3>
+        <div className="flex items-center mb-1 pr-6">
+          <h3 className="font-medium text-3xl text-[#0e423e]">500</h3><Plus className="text-[#0e423e]" size={30}/>
         </div>
 
         {/* Single benefit */}
         <p className="text-sm text-[#0e423e] text-left mb-4">
-          Without Spending a Dollar
+          How we got our first 500 users without running Ads
         </p>
 
         {/* CTA Button */}
         <button href="https://medium.com/@talktoolumide/it-started-with-a-familiar-pain-point-f33f26b85d3a" className="w-full hover:from-green-600 hover:to-emerald-700 border border-[#0e423e] hover:bg-[#8eda91] text-[#0e423e]
          text-sm font-medium rounded-sm px-2 py-0.5 transition-all duration-200 hover:shadow-green-500/25 ring-1 ring-green-400/20">
-          Learn How
+          Read Article
 
         </button>
 
+
         {/* Price */}
-        <div className="text-left px-2.5 py-0.5 bg-black/20 rounded-full !font-medium text-xs text-[#132a29] w-fit -mb-1 mt-4">
+        <div className="text-left px-2.5 py-0.5 hidden bg-black/20 rounded-full !font-medium text-xs text-[#132a29] w-fit -mb-1 mt-4">
           Article on medium
         </div>
+
+      </div>
+      <img
+        src="/advert-3.png"
+        alt="Envoyce Logo"
+        className="w-full bg-cover bg-green-500"
+      />
       </div>
 
       {/* Upgrade Mobile */}
